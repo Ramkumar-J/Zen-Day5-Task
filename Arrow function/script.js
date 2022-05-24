137f78749048b791dffba4145e9e27a0388ca91c
ramkumar <ramviru9@gmail.com>
2022-05-24 17:51:59 +0530

    commit

--- Arrow function/script.js
+++ Arrow function/script.js
@@ -1,6 +1,5 @@
 // a.Print odd numbers in an array
-let oddArray=[2,3,6,7,8];
-let arr=[1,3,5,8,10];
+let arr=[2,3,6,7,8];
 let oddNumbers= (input) =>
 {
    for(let i=0;i<input.length;i++)
@@ -11,10 +10,17 @@ let oddNumbers= (input) =>
        }
    }
 }
-oddNumbers(oddArray);
 oddNumbers(arr);
 
 // c. Sum of all numbers in an array
 let numArray=[1,4,6,3,8];
 let sum=numArray.reduce((previousValue,currentValue) => previousValue+currentValue,0);
 console.log(sum);
+
+// e. Return all the palindromes in an array
+let words=["ramar","raj","lol"]
+let checkPalindrom= (word) => { 
+    return word == word.split('').reverse().join('');
+}
+const result = words.filter(word => checkPalindrom(word));
+console.log(result);
--- function/script.js
+++ function/script.js
@@ -1,5 +1,5 @@
 // a.Print odd numbers in an array
-let oddArray=[2,3,6,7,8];
+let arr=[2,3,6,7,8];
 function oddNumbers(input)
 {
    for(let i=0;i<input.length;i++)
@@ -10,7 +10,7 @@ function oddNumbers(input)
        }
    }
 }
-oddNumbers(oddArray);
+oddNumbers(arr);
 // c. Sum of all numbers in an array
 let numArray=[1,4,6,3,8];
 let sum=numArray.reduce(function(previousValue,currentValue)
@@ -18,3 +18,21 @@ let sum=numArray.reduce(function(previousValue,currentValue)
     return (previousValue+currentValue);
 },0);
 console.log(sum);
+
+let array = [3, 12, 50, 23, 0];
+    array.forEach(function (element) {
+        const isPrime =(element);
+        if (isPrime) {
+          console.log(`${element} is a prime number`);
+        } else {
+          console.log(`${element} is NOT a prime number`);
+        }
+      });
+
+// e. Return all the palindromes in an array
+let words=["ramar","raj","lol"]
+function checkPalindrom(word) { 
+    return word == word.split('').reverse().join('');
+}
+const result = words.filter(word => checkPalindrom(word));
+console.log(result);
